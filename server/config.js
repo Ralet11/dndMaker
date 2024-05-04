9a6de3860dd8660f6e0a53ec3c21d08e0dbe9a5d
Apptrader <proyectoapptrader@gmail.com>
2024-05-03 23:11:45 -0300

    sadasd

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-export const TOKEN_SECRET = 'some secret key'
+/* export const TOKEN_SECRET = 'some secret key'
 
 export const DB_HOST = 'localhost'
 export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
@@ -6,3 +6,12 @@ export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
 export const DB_DATABASE =process.env.DB_DATABASE || 'dnd'
 export const DB_USER = process.env.DB_USER || 'postgres'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'admin'
+ */
+export const TOKEN_SECRET = 'some secret key'
+
+export const DB_HOST = 'aiqdnd.cryueqiccrnl.us-east-2.rds.amazonaws.com'
+export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://aiqfrontend.s3-website.us-east-2.amazonaws.com/'
+
+export const DB_DATABASE =process.env.DB_DATABASE || 'dnd'
+export const DB_USER = process.env.DB_USER || 'postgres'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'dndMaker'
